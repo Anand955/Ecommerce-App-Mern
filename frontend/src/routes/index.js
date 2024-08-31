de08c79462e3553fdb045fc5a533a70d46bb66f5
Anand955 <coderanand23@gmail.com>
2024-08-30 22:05:10 -0700

    gfc

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default router;
